d4fd0e3cf3d5d0f1ad22041da1ec6df2f967cda3
Rahul kumar <126283928+krahulsahu@users.noreply.github.com>
2026-08-21 11:50:01 +0530

    Time: 3 ms (89.40%) | Memory: 48.1 MB (91.37%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total =0;
+
+        for(int i=0; i<timeSeries.length-1; i++){
+            int gap = timeSeries[i+1] - timeSeries[i];
+            total += Math.min(duration, gap);
+            
+        }
+        if(timeSeries.length>0){
+            total += duration;
+        }
+        return total;
+
+    }
+}
